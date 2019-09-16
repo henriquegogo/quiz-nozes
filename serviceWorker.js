43d0daa29b24e5ae58a9c444692946a24ad1e32b
Henrique Gogó <henriquegogo@gmail.com>
2019-09-15 23:48:49 -0300

    Caching all application

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,27 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
+      return cache.addAll([
+        '/',
+        'index.html',
+        'images/mascot.png',
+        'stylesheets/App.css',
+        'stylesheets/Header.css',
+        'stylesheets/index.css',
+        'stylesheets/Level.css',
+        'stylesheets/Modal.css',
+        'stylesheets/Report.css',
+        'lib/nozes/nozes.js',
+        'javascripts/ApiService.js',
+        'javascripts/App.js',
+        'javascripts/Categories.js',
+        'javascripts/Header.js',
+        'javascripts/index.js',
+        'javascripts/Level.js',
+        'javascripts/Modal.js',
+        'javascripts/Report.js',
+        'javascripts/Trivia.js'
+      ]);
     })
   );
 });
